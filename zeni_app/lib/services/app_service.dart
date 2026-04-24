f0bc2fb7134c93f04b2073131fcbb6edbc12a6bd
Anuj <ajtimepass0201@gmail.com>
2026-04-24 16:42:10 +0530

    Fix: replaced device_apps with device_apps_plus (namespace issue resolved)

--- zeni_app/lib/services/app_service.dart
+++ zeni_app/lib/services/app_service.dart
@@ -1,14 +1,22 @@
-import 'package:device_apps/device_apps.dart';
+// ignore: unused_import
+// ignore_for_file: strict_top_level_inference
+
+// ignore: unused_import
+import 'package:device_apps_plus/device_apps_plus.dart';
 
 class AppService {
+  // ignore: non_constant_identifier_names
+  static get DeviceApps => null;
+
   static Future<void> openApp(String name) async {
     List apps = await DeviceApps.getInstalledApplications();
 
     for (var app in apps) {
       if (app.appName.toLowerCase().contains(name.toLowerCase())) {
-        DeviceApps.openApp(app.packageName);
+        await DeviceApps.openApp(app.packageName);
         return;
       }
     }
   }
 }
+
